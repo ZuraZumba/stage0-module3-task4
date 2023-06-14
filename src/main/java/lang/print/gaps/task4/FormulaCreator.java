716012d280c87bc9bb2ac6a5634351cf5cf6623d
ZuraZumba <zura.zumbadze@yahoo.com>
2023-06-14 15:55:36 +0400

    4-1

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=0, b=0;
+        long power = (int)  Math.pow(2, a);
+        long leftSide = (9*power - 5*b + 2 + a - 7);
+        long rightside = (a+b-4*a*b)/2;
+        long result = leftSide*rightside;
     }
 }
