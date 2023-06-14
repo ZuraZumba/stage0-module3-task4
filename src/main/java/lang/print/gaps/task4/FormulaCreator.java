780db351b658fce558aaa131e415b84655017f76
zurazumba <zura.zumbadze@yahoo.com>
2023-06-15 00:31:40 +0400

    4-2

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,10 +2,13 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a=0, b=0;
-        long power = (int)  Math.pow(2, a);
-        long leftSide = (9*power - 5*b + 2 + a - 7);
-        long rightside = (a+b-4*a*b)/2;
-        long result = leftSide*rightside;
+        int a=3;
+        int b=2;
+        double res = (int) ((9*Math.pow(a, 2) - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2));
+        System.out.println(res);
+//        long power = (int)  Math.pow(2, a);
+//        long leftSide = (9*power - 5*b + 2 + a - 7);
+//        long rightside = (a+b-4*a*b)/2;
+//        long result = leftSide*rightside;
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,22 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-         long first = 10_000_000_000L;
-         char second = 'a';
-         boolean third = false;
-         double forth = 12.90434323324342;
-         float fifth = 12.3F;
-         byte sixth = 1;
-         short seventh = 129;
+        long first = 10_000_000_000L;
+        char second = 'a';
+        boolean third = false;
+        double forth = 12.90434323324342;
+        float fifth = 12.3F;
+        byte sixth = 1;
+        short seventh = 129;
+
+        // Use the variables in some way
+        System.out.println("first: " + first);
+        System.out.println("second: " + second);
+        System.out.println("third: " + third);
+        System.out.println("forth: " + forth);
+        System.out.println("fifth: " + fifth);
+        System.out.println("sixth: " + sixth);
+        System.out.println("seventh: " + seventh);
     }
 }
+
